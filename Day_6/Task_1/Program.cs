d2809dc6650ee659725600f0a1e34551a357856e
MohammedCS <mohammed.cis.cs@gmail.com>
2022-09-22 02:55:01 +0200

    add Day_6

---
+++ Day_6/Task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Enter Your Number : ");
+			int num = Convert.ToInt32(Console.ReadLine());
+
+			while(num != 0)
+			{
+				Console.WriteLine(num % 10);
+				num /= 10;
+			}
+		}
+	}
+}
---
+++ Day_6/Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Enter Your Num : ");
+			int num = Convert.ToInt32(Console.ReadLine());
+
+			int temp = 1;
+			while(num != 0)
+			{
+				Console.WriteLine((num % 10) * temp);
+				num /= 10;
+				temp *= 10;
+			}
+		}
+	}
+}
