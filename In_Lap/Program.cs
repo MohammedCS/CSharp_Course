7b82c466a2c27063d4c5ff0d650f6ffd11a8268a
MohamedCS <mohammed.cis.cs@gmail.com>
2022-09-22 02:27:37 +0200

    Upload The Files

--- In_Lap/Program.cs
+++ In_Lap/Program.cs
@@ -6,7 +6,16 @@ namespace In_Lap
 	{
 		static void Main(string[] args)
 		{
-			
+			Console.Write("Enter num : ");
+			int num = Convert.ToInt32(Console.ReadLine());
+			int count = 1;
+
+			while(num != 0)
+			{
+				Console.WriteLine((num % 10) * count);
+				count *= 10;
+				num /= 10;
+			}
 		}
 	}
 }
