1d7bf346effeb79e9fea210bc28d496249b5d98b
MohammedCS <mohammed.cis.cs@gmail.com>
2022-09-24 12:53:37 +0200

    add some code

---
+++ Book/Chapter_2/Play_Ground/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+#error version
+
+namespace Play_Ground
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
--- Play_Ground/Program.cs
+++ Play_Ground/Program.cs
@@ -6,7 +6,48 @@ namespace Play_Ground
 	{
 		static void Main(string[] args)
 		{
+			Console.Write("Enter you name : ");
+			string name = Console.ReadLine();
 			
+			Console.Write("Enter your degree in arabic : ");
+			int arabic = Convert.ToInt32(Console.ReadLine());
+
+			Console.Write("Enter your degree in English : ");
+			int English = Convert.ToInt32(Console.ReadLine());
+
+			Console.Write("Enter your degree in math : ");
+			int math = Convert.ToInt32(Console.ReadLine());
+
+			Console.Write("Enter your degree in science : ");
+			int science = Convert.ToInt32(Console.ReadLine());
+
+			int total = arabic + English + math + science;
+			
+			if (total < 50 && total >= 0)
+			{
+				Console.WriteLine(name + "'s degree is F");
+			}
+			else if (total >= 50 && total < 65)
+			{
+				Console.WriteLine(name + "'s degree is D");
+			}
+			else if (total >= 65 && total < 75)
+			{
+				Console.WriteLine(name + "'s degree is B");
+			}
+			else if (total >= 75 && total < 85)
+			{
+				Console.WriteLine(name + "'s degree is B+");
+			}
+			else if (total >= 85 && total <= 100)
+			{
+				Console.WriteLine(name + "'s degree is A");
+			}
+			else
+			{
+				Console.WriteLine("Wrong Number");
+			}
+			Console.ReadKey();
 		}
 	}
 }
