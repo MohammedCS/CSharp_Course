a034f9baa0bc3cb281ab197795494e84f7565c8c
MohammedCS <mohammed.cis.cs@gmail.com>
2022-09-26 02:04:38 +0200

    Add the new task

--- Day_7/Program.cs
+++ Day_7/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Task_1
+namespace Task
 {
-	class Program
+	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			
+	        int [] array = new int[5];
+            Console.WriteLine("Please Enter 5 numbers");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("Enter num_" + (i + 1) + " : ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < array.Length; i++) 
+            {
+                double cubed = Math.Pow(array[i], 3);
+                Console.WriteLine("Number is : " + array[i] + " and cube of it is : " + cubed);
+            }
+
+            Console.ReadKey();
 		}
 	}
 }
